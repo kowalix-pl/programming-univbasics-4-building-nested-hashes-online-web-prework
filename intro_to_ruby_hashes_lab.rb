d9385edee639d34c0fabe673639d65ecd122698e
kowalix007 <kowalix007@gmail.com>
2019-11-14 23:28:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ def monopoly_with_third_tier
   # complexity.
   
   monopoly={:railroads =>{:names =>{:reading_railroad ={},:pennsylvania_railroad ={},:b_and_o_railroad = {},:shortline_railroad => {}},:pieces =>4,:rent_in_dollars =>{:one_piece_owned =>{},:two_pieces_owned =>{},:three_pieces_owned =>{},:four_pieces_owned =>{},
-  }}} 
+  }}}} 
     
   end
 
