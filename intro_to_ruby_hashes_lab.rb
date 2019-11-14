6d14a5a95d89393558dbdee5dee5af0a3b483980
kowalix007 <kowalix007@gmail.com>
2019-11-14 22:59:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,7 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   monopoly={:railroads =>{:rent_in_dollars =>{:pieces =>4}}}
-
+  [monopoly][:railroads][:rent_in_dollars].keys.count = 4
   end
 
 def monopoly_with_fourth_tier
